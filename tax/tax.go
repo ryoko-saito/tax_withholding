a84610c2aa56e934c6c63e1892863cd8e526128d
Ryoko Saito <ryoko@saitodev.co>
2018-10-19 10:38:11 +0900

    源泉徴収

--- tax/tax.go
+++ tax/tax.go
@@ -1,46 +1,18 @@
 package tax
 
 type TaxSet struct {
-	index int
-	tax   int
+	index int   //(月収-社会保険)/1000
+	kou   []int //甲
+	otsu  int   //乙
 }
 
-//450万以上の税金額
-const maxTax = 21800
-
-func NewList() []TaxSet {
+func NewTaxList() []TaxSet {
 	list := []TaxSet{
-		TaxSet{100, 6900},
-		TaxSet{150, 7800},
-		TaxSet{200, 10300},
-		TaxSet{250, 11800},
-		TaxSet{300, 13300},
-		TaxSet{350, 14800},
-		TaxSet{400, 16800},
-		TaxSet{450, 18800},
+		TaxSet{89, []int{130, 0, 0, 0, 0, 0, 0, 0}, 3200},
+		TaxSet{90, []int{180, 0, 0, 0, 0, 0, 0, 0}, 3200},
+		TaxSet{91, []int{230, 0, 0, 0, 0, 0, 0, 0}, 3200},
+		TaxSet{92, []int{290, 0, 0, 0, 0, 0, 0, 0}, 3200},
+		TaxSet{93, []int{340, 0, 0, 0, 0, 0, 0, 0}, 3300},
 	}
 	return list
-}
-
-//1000000
-func Calc(s int, age int) int {
-	ss := int(s / 10000)
-	list := NewList()
-
-	var total int
-
-	//40歳以上
-	if age >= 40 {
-		total = 3300
-	}
-
-	min := 0
-	for _, set := range list {
-		if ss >= min && ss < set.index {
-			total += set.tax + 3200
-			return total
-		}
-		min = set.index
-	}
-	return total + maxTax + 3200
 }
