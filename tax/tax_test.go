2f6179374c6c7b8f4842aa2e9659d6b859a45798
Ryoko Saito <ryoko@saitodev.co>
2018-10-23 12:05:47 +0900

    源泉徴収途中

--- tax/tax_test.go
+++ tax/tax_test.go
@@ -1,1 +1,30 @@
 package tax
+
+import (
+	"testing"
+)
+
+func TestCalcTax(t *testing.T) {
+	income := 200000
+	kou_or_otsu := 0
+	support := 0
+
+	tax := CalcTax(income, kou_or_otsu, support)
+	if tax != 4770 {
+		t.Error("failed")
+	}
+
+	support = 5
+	tax = CalcTax(income, kou_or_otsu, support)
+	if tax != 0 {
+		t.Error("failed")
+	}
+
+	income = 478000
+	kou_or_otsu = 0
+	support = 5
+	tax = CalcTax(income, kou_or_otsu, support)
+	if tax != 7500 {
+		t.Error("failed")
+	}
+}
